6e520e0dfb41c9925ed2a9b69fc7f3b8952644f1
Filip Klimeš <filip@filipklimes.cz>
2018-03-15 13:56:20 +0100

    add missing todo in java

--- java/business-context/src/main/java/cz/filipklimes/diploma/framework/businessContext/loader/LocalBusinessContextLoader.java
+++ java/business-context/src/main/java/cz/filipklimes/diploma/framework/businessContext/loader/LocalBusinessContextLoader.java
@@ -10,6 +10,7 @@ public class LocalBusinessContextLoader implements BusinessContextLoader
     @Override
     public Set<BusinessContext> load()
     {
+        // TODO: implement
         throw new UnsupportedOperationException("not implemented yet");
     }
 
